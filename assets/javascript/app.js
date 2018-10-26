0808dca454ae29340913692c45de365b92c8a1c2
dancrump1 <42187710+dancrump1@users.noreply.github.com>
2018-10-26 12:24:08 -0400

    bugs with userInput

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,39 @@
 var gifyAPI; 
-var userSearchInput ;
+var userSearchInput = [] ;
+
+
+
+//KNOWN BUGS:
+//BUTTONS VALUES GET OVERWRITTEN BY NEWEST MADE BUTTON
+//NEED TO STORE LOCAL MEMORY
+
+
+//BUGS COME FROM userSearchInput
+
+
+
+
+function createButton(){
+    var newButton = $("<button>"+userSearchInput+"</button>").attr("class","addedGifButton");
+    $(".addedGifButton").attr("value",userSearchInput);
+    $("#newButtonSpot").append($(newButton));    
+
+};
+
 
 $("#add-search").on("click",function(){
-    userSearchInput= $("#userEntry").val();
-    var newButton = $("<button>"+userSearchInput+"</button>").attr("id","addedGifButton");
-    $("#addedGifButton").attr("value",userSearchInput);
-    $("#newButtonSpot").append($(newButton));
+    userSearchInput= $(".userEntry").val();
+    createButton();
+    
 })
 
-$(document).on("click", "#addedGifButton", function(){
+$(document).on("click", ".addedGifButton", function(){
 
     var gifDiv = $("#imagesGoHere").empty();
+    userSearchInput = $(".userEntry").val();
+    
 
-    gifyAPI  = "http://api.giphy.com/v1/gifs/search?q="+userSearchInput+"&api_key=qr9eTBdtXgyT0eIUBLPwKsSA5IvgDtxF&limit=5";
+    gifyAPI  = "http://api.giphy.com/v1/gifs/search?q="+userSearchInput+"&api_key=qr9eTBdtXgyT0eIUBLPwKsSA5IvgDtxF&limit=15";
     $.ajax({
         url: gifyAPI,
         method:"GET"
@@ -21,11 +42,16 @@ $(document).on("click", "#addedGifButton", function(){
     
        
         for (var i = 0; i < response.data.length; i++){
-            var results= response.data[i]
-            var gifHolder = $("<img>").attr("src", results.images.fixed_height.url)
+            var results= response.data[i];
+            var gifandratingHolder=$("<div>").attr("id", "holdsGifsAndRating");
+            var gifHolder = $("<img>").attr("src", results.images.fixed_height.url);
+            gifHolder.attr("style" ,"width:200px;height:180px;")
             var ratingText = $("<p>").text("Rating: "+results.rating)
-            gifDiv.append(gifHolder);
-            gifDiv.append(ratingText);
+            gifandratingHolder.append(ratingText);
+            gifandratingHolder.append(gifHolder);
+            gifDiv.append(gifandratingHolder);
+
+
          }
    })
 
